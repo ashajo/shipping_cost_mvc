405b4cb7f3ecc0509324c2e3b5a45e3fb8938e9f
asha joseph <ashajo183@gmail.com>
2021-04-13 23:14:26 +0200

    Mapping done on the ShippingController Class+thymeleaf namespace+bootstrap resources added to html page and a form created inside addBoxForm html page

--- src/main/java/se/lexicon/shipping_cost_mvc/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost_mvc/controller/ShippingController.java
@@ -1,4 +1,15 @@
 package se.lexicon.shipping_cost_mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/shipping")
 public class ShippingController {
+
+@GetMapping("/boxForm")
+public String showBoxForm(){
+    return "addBoxForm";
+}
 }
