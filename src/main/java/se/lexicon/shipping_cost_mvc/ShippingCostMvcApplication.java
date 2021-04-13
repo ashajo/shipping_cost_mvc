a8de639a2c3e41d1a1268f77b206345540f6104a
asha joseph <ashajo183@gmail.com>
2021-04-13 21:47:45 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/shipping_cost_mvc/ShippingCostMvcApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.shipping_cost_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingCostMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingCostMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/shipping_cost_mvc/ShippingCostMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.shipping_cost_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingCostMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
