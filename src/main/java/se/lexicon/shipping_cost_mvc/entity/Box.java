e7369efd1fc9f246ebbbfe4533a03aa3ecedfcbd
asha joseph <ashajo183@gmail.com>
2021-04-13 22:32:11 +0200

    Entity added to Box Class

--- src/main/java/se/lexicon/shipping_cost_mvc/entity/Box.java
+++ src/main/java/se/lexicon/shipping_cost_mvc/entity/Box.java
@@ -1,16 +1,39 @@
 package se.lexicon.shipping_cost_mvc.entity;
 
-import java.time.LocalDateTime;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+@Entity
 public class Box {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(nullable = false,updatable = false)
+
     private String id;
+    @Column(nullable = false,length = 200)
     private String name;
+    @Column(nullable = false,length = 200)
+
     private String country;
+    @Column(nullable = false,length = 200)
     private String type;
+    @Column(nullable = false)
     private double cost;
+    @Column(nullable = false)
     private double weight;
+    @Column(nullable = false,length = 200)
     private String weightType;
+    @CreationTimestamp
+    @Column(nullable = false,columnDefinition = "TIMESTAMP default CURRENT_TIMESTAMP")
     private LocalDateTime createDate;
+    @Column(nullable = false,columnDefinition = "tinyint(1) default 1")
     private boolean status;
 
     public Box() {
